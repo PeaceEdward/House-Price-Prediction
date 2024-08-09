030d3af36a03ae854b2a6dac9cd17854e52a9839
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2024-08-09 10:41:28 +0100

    Update predictor_app.py

--- predictor_app.py
+++ predictor_app.py
@@ -3,8 +3,7 @@ import joblib
 import pandas as pd
 import os
 import pickle
-pip install catboost
-import catboost
+from catboost import CatBoostRegressor
 
 model_path = os.path.abspath(os.path.join(os.getcwd(), "catboost_model.pkl"))
 scaler_path = os.path.abspath(os.path.join(os.getcwd(), "Scaler.pkl"))
